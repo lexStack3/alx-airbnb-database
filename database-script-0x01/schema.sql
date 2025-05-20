4f88d755ca9dd57b5d1124eeca368a08d58be5a3
lexStack3 <alexanderedim80@gmail.com>
2025-05-20 15:19:44 +0100

    AirBnB - MySQL Database Schema

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,77 @@
+-- Creates User Table
+CREATE TABLE IF NOT EXISTS `User` (
+    user_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) DEFAULT NULL,
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX (user_id)
+    ) ENGINE=InnoDB;
+
+-- Creates Property Table
+CREATE TABLE IF NOT EXISTS `Property` (
+    property_id CHAR(36) PRIMARY KEY,
+    host_id CHAR(36),
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    pricepernight DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (host_id) REFERENCES `User`(user_id) ON DELETE CASCADE,
+    INDEX (property_id)
+    ) ENGINE=InnoDB;
+
+-- Creates Booking Table
+CREATE TABLE IF NOT EXISTS `Booking` (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES `Property`(property_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES `User`(user_id) ON DELETE CASCADE,
+    INDEX (booking_id)
+    ) ENGINE=InnoDB;
+
+-- Creates Payment Table
+CREATE TABLE IF NOT EXISTS `Payment` (
+    payment_id CHAR(36) PRIMARY KEY,
+    booking_id CHAR(36),
+    amount DECIMAL NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking) REFERENCES `Booking`(booking_id) ON DELETE CASCADE,
+    INDEX (payment_id)
+    ) ENGINE=InnoDB;
+
+-- Creates Review Table
+CREATE TABLE IF NOT EXISTS `Review` (
+    review_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36),
+    user_id CHAR(36),
+    rating INT NOT NULL CHECK(rating BETWEEN 1 AND 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property) REFERENCES `Property`(property_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES `User`(user_id) ON DELETE CASCADE,
+    INDEX (review_id)
+    ) ENGINE=InnoDB;
+
+-- Creates Message Table
+CREATE TABLE IF NOT EXISTS `Message` (
+    message_id CHAR(36) PRIMARY KEY,
+    sender_id CHAR(36),
+    recipent_id CHAR(36),
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES `User`(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (recipent_id) REFERENCES `User`(user_id) ON DELETE CASCADE,
+    INDEX (message_id)
+    ) ENGINE=InnoDB;
