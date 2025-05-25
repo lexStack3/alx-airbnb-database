f22d508515b5c026d1099fa5e8e5427be638fd47
lexStack3 <alexanderedim80@gmail.com>
2025-05-25 16:43:23 +0100

    Both correlated and non-corellated subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,12 +1,12 @@
 SELECT
-    Property.property_id AS property_id,
-    Property.name AS property_name,
-    Review.rating AS rating,
-    Review.comment AS comment
+    property_id,
+    name AS property_name
 FROM Property
-INNER JOIN Review
-ON Property.property_id = Review.property_id
-WHERE Review.rating >= 4;
+WHERE (
+    SELECT AVG(rating)
+    FROM Review
+    WHERE Review.property_id = Review.property_id
+) > 4.0;
 
 SELECT
     user_id,
