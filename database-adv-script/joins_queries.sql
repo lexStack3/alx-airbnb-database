73a70b5bdd24b10ad3e61f1b6886f4069e6e2fcc
lexStack3 <alexanderedim80@gmail.com>
2025-05-25 15:47:56 +0100

    Mastering SQL joins by writing complex queries using different types of joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,21 +2,26 @@ SELECT User.user_id AS user_id,
     first_name,
     last_name,
     booking_id,
+    start_date,
+    end_date,
+    total_price,
     status,
     Booking.created_at AS created_at
-FROM Booking
-INNER JOIN User
-ON Booking.user_id = User.user_id;
+FROM User
+INNER JOIN Booking
+ON User.user_id = Booking.user_id;
 
-SELECT Property.property_id,
-    name,
-    description,
-    user_id,
-    rating,
-    comment
+SELECT Property.property_id AS property_id,
+    Property.name AS property_name,
+    Property.location,
+    Review.review_id,
+    Review.rating,
+    Review.comment,
+    Review.created_at
 FROM Property
 LEFT JOIN Review
-ON Property.property_id = Review.property_id;
+ON Property.property_id = Review.property_id
+ORDER BY Property.name ASC;
 
 SELECT
     User.user_id AS user_id,
@@ -44,4 +49,6 @@ SELECT
     status,
     Booking.created_at AS created_at
 FROM Booking
-LEFT JOIN User ON Booking.user_id = User.user_id;
+LEFT JOIN User ON Booking.user_id = User.user_id
+
+ORDER BY first_name ASC;
