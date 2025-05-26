548b40828b23cbd0f1a8cfd78242df14c6e11c74
lexStack3 <alexanderedim80@gmail.com>
2025-05-26 11:39:11 +0100

    Identifies and create indexes to improve query performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,25 @@
+-- Before Indexing
+EXPLAIN ANALYZE SELECT
+    property_id,
+    name AS property_name
+FROM Property
+WHERE (
+    SELECT AVG(rating)
+    FROM Review
+    WHERE Review.property_id = Review.property_id
+) > 4.0;
+
+EXPLAIN ANALYZE SELECT
+    user_id,
+    first_name,
+    last_name
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.user_id
+) > 3;
+
 -- User table
 CREATE INDEX idx_user_first_name ON User(first_name);
 CREATE INDEX idx_user_last_name ON User(last_name);
@@ -13,3 +35,25 @@ CREATE INDEX idx_property_name ON Property(name);
 -- Review table
 CREATE INDEX idx_review_property_id ON Review(property_id);
 CREATE INDEX idx_review_rating ON Review(rating)
+
+-- After Indexing
+EXPLAIN ANALYZE SELECT
+    property_id,
+    name AS property_name
+FROM Property
+WHERE (
+    SELECT AVG(rating)
+    FROM Review
+    WHERE Review.property_id = Review.property_id
+) > 4.0;
+
+EXPLAIN ANALYZE SELECT
+    user_id,
+    first_name,
+    last_name
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.user_id
+) > 3;
